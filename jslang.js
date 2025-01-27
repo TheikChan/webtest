257c61ce9c85a67bf3901ba855c523d00383ba19
Theik Chan <theikchan.tc@gmail.com>
2025-01-27 23:26:06 +0700

    first commit

---
+++ jslang.js
@@ -0,0 +1,33 @@
+
+
+// statement
+
+let myVariable 
+= 
+4;
+
+console.log(typeof(myVariable));
+console.log(typeof(myVariable.toString()));
+
+if(myVariable == 4){
+    console.log("it is even")
+}else {
+    console.log("it is odd")
+}
+
+console.log("1" + 1) // "11" because weakly typed language value no need to explicity marked as specific data type other strongly type language will show compile error
+
+
+// primitive data type
+// Numbers
+
+// number value is made up of series of numeric character
+
+let number = 5;
+
+// number special global properties
+// e.g Infinity and NaN when attempt to perform math calcualtion on non-number values 
+
+Number("11") // convert string into number
+
+
